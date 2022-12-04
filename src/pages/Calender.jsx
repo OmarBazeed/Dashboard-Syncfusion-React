1b3ce9899c8a14c7fe4e0877c004030b61e9d404
Omar Bazeed <omarbazeed@gmail.com>
2022-12-04 18:20:15 +0200

    Adding Calender App Using ScheduleComponent With Some Props

--- src/pages/Calender.jsx
+++ src/pages/Calender.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import {ScheduleComponent , Day , Week , WorkWeek , Inject , Agenda , Resize , Month , DragAndDrop } from '@syncfusion/ej2-react-schedule';
+import { Header } from '../components';
+import {scheduleData} from '../data/dummy'
 
 const Calender = () => {
   return (
-    <div>Calender</div>
+    <div className='calenderPage pt-3 mx-3 ps-3' style={{background:'white'}}>
+
+    <Header category='App' title='Calender' />
+
+    <ScheduleComponent
+    height='650px'
+    eventSettings={{ dataSource: scheduleData}}
+    selectedDate={new Date("2022, 9 , 12")}
+    >
+
+      <Inject services={[Day , Week , WorkWeek , Month , Agenda, Resize , DragAndDrop]} />
+    
+    </ScheduleComponent>
+
+    </div>
   )
 }
 
