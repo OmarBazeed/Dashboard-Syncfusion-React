31c14f55f3cf8218af975c2fff35510c629859e1
Omar Bazeed <omarbazeed@gmail.com>
2022-12-04 02:56:26 +0200

    Adding Employees Page With Sync Component GridComponent The Same In Orders Page With Doiffrenet Inject Props

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import {GridComponent , ColumnsDirective , ColumnDirective , Page , Search , Toolbar , Inject} from '@syncfusion/ej2-react-grids';
+import {employeesData  , employeesGrid} from '../data/dummy';
+import { Header } from '../components';
 
 const Employees = () => {
   return (
-    <div>Employees</div>
+    <div className='employessPage ps-3 pt-3' style={{background:'white',}}>
+
+    <Header category='Page' title='Employees' />
+
+    <GridComponent
+      dataSource={employeesData}
+      allowPaging
+      allowSorting
+      toolbar={['search']}
+      width='auto'
+    >
+
+      <ColumnsDirective>
+        {employeesGrid.map((item,index)=>(
+          <ColumnDirective key={index} {...item} />
+        ))}
+      </ColumnsDirective>
+    
+      <Inject services={[Page, Search , Toolbar]} />
+
+    </GridComponent>
+
+    </div>
   )
 }
 
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {GridComponent , ColumnsDirective , ColumnDirective ,  Resize , Sort , ContextMenu , Filter , Page , ExcelExport , PdfExport , Edit , Inject} from '@syncfusion/ej2-react-grids';
-import {ordersData , contextMenuItems , ordersGrid} from '../data/dummy';
+import {ordersData  , ordersGrid} from '../data/dummy';
 import { Header } from '../components';
 
 
 const Orders = () => {
   return (
-    <div className='ps-3 pt-3' style={{background:'white',}}>
+    <div className='ordersPage ps-3 pt-3' style={{background:'white',}}>
 
     <Header category='Page' title='Orders' />
 
