a2b347e45a2f9c5d03cd5e9cee2309672a168358
Omar Bazeed <omarbazeed@gmail.com>
2022-12-08 21:00:23 +0200

    Finally Finished Bar Page Using Syncfusion ChartComponent With Some Props In Inject />

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,8 +1,9 @@
 import  React , {useContext} from "react";
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, Tooltip, ColumnSeries, DataLabel } from '@syncfusion/ej2-react-charts';
+import {barCustomSeries , barPrimaryXAxis ,barPrimaryYAxis } from '../../data/dummy';
 import {Header} from '../../components';
 import { Context } from '../../contexts/ContextProvider';
-import {barCustomSeries , barPrimaryXAxis ,barPrimaryYAxis } from '../../data/dummy';
+
 
 
 const Bar = ()=>{
@@ -10,9 +11,9 @@ const Bar = ()=>{
   const {currentMode} = useContext(Context)
 
   return (
-    <div className="barPage p-3" style={{background : currentMode === 'Light' ? 'white' :'black'}} >
+    <div className="barPage p-3" style={{background : currentMode === 'Light' ? 'white' : '#bdbdbd'}} >
 
-      <Header category="Charts" title='anything' />
+      <Header category="Charts" title='Bar Chart' />
 
       <ChartComponent
       id="charts"
@@ -20,16 +21,17 @@ const Bar = ()=>{
       primaryYAxis={barPrimaryYAxis}
       tooltip={{enable: true}}
       chartArea={{border:{ width:0 }}}
+      background={currentMode === 'Light' ? '#fff' : '#33373E'}
+      title="Olympics Medal Counts - RIO"
+      style={{textAlign: 'center'}}
       >
-      
-      <SeriesCollectionDirective>
-      {barCustomSeries.map((item , index)=>(
-        <SeriesDirective key={index} {...item}  />
-      ))}
-      </SeriesCollectionDirective>
-      
-      <Inject services={[Legend, Category, Tooltip, ColumnSeries, DataLabel]} />
-
+        <SeriesCollectionDirective>
+          {barCustomSeries.map((item , index)=>(
+            <SeriesDirective key={index} {...item}  />
+          ))}
+        </SeriesCollectionDirective>
+        
+        <Inject services={[Legend, Category, Tooltip, ColumnSeries, DataLabel]} />
       </ChartComponent>
 
     </div>
