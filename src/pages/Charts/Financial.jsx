dce0c06d0a490e07fcfa641ad5dfe5acbfc569b7
Omar Bazeed <omarbazeed@gmail.com>
2022-12-09 04:59:33 +0200

    Adding Financial Page Using Syncfusion ChartComponent With Some Props In Inject /> and SeriesDirective Which Carry The DataSource And Other Props Like low, high

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React ,{useContext} from 'react';
+import {ChartComponent , SeriesCollectionDirective , SeriesDirective , Inject , HiloSeries , DateTime , Tooltip , Crosshair  } from '@syncfusion/ej2-react-charts';
+import {financialChartData , FinancialPrimaryXAxis , FinancialPrimaryYAxis } from '../../data/dummy';
+import { Header } from '../../components';
+import { Context } from '../../contexts/ContextProvider';
 
 const Financial = () => {
+  const {currentMode} = useContext(Context);
   return (
-    <div>Financial</div>
+    <div className='areaPage p-3' style={{background: currentMode === 'Light' ? 'white' : '#bdbdbd'}}>
+
+      <Header category='Charts' title='Financial' />
+
+      <ChartComponent
+      title='AAPL Historical'
+      primaryXAxis={FinancialPrimaryXAxis}
+      primaryYAxis={FinancialPrimaryYAxis}
+      tooltip={{enable: true}}
+      crosshair={{enable: true , lineType:'Vertical'}}
+      >
+      
+        <SeriesCollectionDirective>
+        <SeriesDirective dataSource={financialChartData} type='Hilo' xName='x' high='high' low='low' name='Apple Inc' >
+        
+        </SeriesDirective>
+        </SeriesCollectionDirective>
+        
+        <Inject services={[Tooltip , HiloSeries , DateTime , Crosshair]} />
+
+      </ChartComponent>
+    
+    </div>
   )
 }
 
