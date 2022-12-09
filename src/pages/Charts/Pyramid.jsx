728ff80917a05a9dad15531f0c9347e78baed6be
Omar Bazeed <omarbazeed@gmail.com>
2022-12-09 05:09:10 +0200

    Adding Pyramid Page Using Syuncfusion  ChartComponent With The Same Steps In The Pie Page With The ChartComponent With All Props In Inject And SeriesDirective

--- src/pages/Charts/Pyramid.jsx
+++ src/pages/Charts/Pyramid.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useContext }  from 'react';
+import {AccumulationChartComponent , AccumulationSeriesCollectionDirective, AccumulationSeriesDirective ,PyramidSeries, Inject , AccumulationLegend , AccumulationDataLabel , AccumulationTooltip} from '@syncfusion/ej2-react-charts';
+import {PyramidData} from '../../data/dummy';
+import { Context } from '../../contexts/ContextProvider';
+import { Header } from '../../components';
+
+
 
 const Pyramid = () => {
+
+const {currentMode} = useContext(Context)
+
   return (
-    <div>Pyramid</div>
+    <div className='pyramidPage p-3' style={{background: currentMode === 'Light' ? 'white' : '#bdbdbd'}}>
+
+      <Header category='Charts' title='Pyramid' />
+
+      <AccumulationChartComponent
+      id='charts'
+      title='Food Comparison Chart'
+      legendSettings={{position:'Top'}}
+      tooltip={{enable:true}}
+      >
+
+        <AccumulationSeriesCollectionDirective>
+          <AccumulationSeriesDirective dataSource={PyramidData} type='Pyramid' xName='x' yName='y' dataLabel={{visible:true , name:'text' , position:'Inside'}}>
+          </AccumulationSeriesDirective>
+        </AccumulationSeriesCollectionDirective>
+
+        <Inject services={[PyramidSeries , AccumulationLegend, AccumulationDataLabel ,AccumulationTooltip]} />
+
+      </AccumulationChartComponent>
+
+      </div>
   )
 }
 
-export default Pyramid
+export default Pyramid;
