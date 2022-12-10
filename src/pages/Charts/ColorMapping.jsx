72bdc54b1d547e99cd353f6dd43de967eba6ac22
Omar Bazeed <omarbazeed@gmail.com>
2022-12-10 04:49:43 +0200

    Adding The Colo-Mapping Page Using Syncfusion ChartComponent-RangeColorSettingsDirective-RangeColorSettingDirective With Some Props In Inject /> And In The SeriesDirective

--- src/pages/Charts/ColorMapping.jsx
+++ src/pages/Charts/ColorMapping.jsx
@@ -1,5 +1,5 @@
 import  React , {useContext} from "react";
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, Tooltip, RangeColumnSeries , DataLabel } from '@syncfusion/ej2-react-charts';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, Tooltip , ColumnSeries , RangeColorSettingsDirective, RangeColorSettingDirective   } from '@syncfusion/ej2-react-charts';
 import {colorMappingData , ColorMappingPrimaryXAxis ,ColorMappingPrimaryYAxis ,rangeColorMapping } from '../../data/dummy';
 import {Header} from '../../components';
 import { Context } from '../../contexts/ContextProvider';
@@ -24,15 +24,29 @@ const ColorMapping = ()=>{
       background={currentMode === 'Light' ? '#fff' : '#33373E'}
       title="Colored Mapping For The Project Data"
       style={{textAlign: 'center'}}
-      dataSource={rangeColorMapping}
+      legendSettings={{mode:'Range'}}
       >
         <SeriesCollectionDirective>
-          {colorMappingData[0].map((item , index)=>(
-            <SeriesDirective key={index} {...item}  />
-          ))}
+          <SeriesDirective 
+          dataSource={colorMappingData[0]}
+          name='USA'
+          xName='x'
+          yName='y'
+          type='Column'
+          cornerRadius={{
+            topLeft:10 ,
+            topRight:10, 
+          }}
+          />
         </SeriesCollectionDirective>
-        
-        <Inject services={[Legend, Category, Tooltip, RangeColumnSeries , DataLabel]} />
+
+        <RangeColorSettingsDirective>
+          {rangeColorMapping.map((item , index)=>(
+            <RangeColorSettingDirective key={index} {...item}  />
+          ))}
+        </RangeColorSettingsDirective>
+
+        <Inject services={[Legend, Category, Tooltip, ColumnSeries ]} />
       </ChartComponent>
 
     </div>
