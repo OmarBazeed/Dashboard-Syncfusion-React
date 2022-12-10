fa7b506f7cbaaaa1bdc3c481f5d951db481c32e2
Omar Bazeed <omarbazeed@gmail.com>
2022-12-10 03:30:34 +0200

    Adding StackedCoumnSeries Using Syncfusion ChartComponent With Some Props In The Inject /> Like StackingColumnSeries All These Steps Are The Same Of Creating The Column Or Bar Component

--- src/pages/Charts/Stacked.jsx
+++ src/pages/Charts/Stacked.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import  React , {useContext} from "react";
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, Tooltip, StackingColumnSeries , DataLabel } from '@syncfusion/ej2-react-charts';
+import {stackedCustomSeries , stackedPrimaryXAxis ,stackedPrimaryYAxis } from '../../data/dummy';
+import {Header} from '../../components';
+import { Context } from '../../contexts/ContextProvider';
 
-const Stacked = () => {
+
+
+const Stacked= ()=>{
+
+  const {currentMode} = useContext(Context)
+
   return (
-    <div>Stacked</div>
+    <div className="barPage p-3" style={{background : currentMode === 'Light' ? 'white' : '#bdbdbd'}} >
+
+      <Header category="Charts" title='Stacked' />
+
+      <ChartComponent
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      tooltip={{enable: true}}
+      chartArea={{border:{ width:0 }}}
+      background={currentMode === 'Light' ? '#fff' : '#33373E'}
+      title="Project Cost Breakdown"
+      style={{textAlign: 'center'}}
+      >
+        <SeriesCollectionDirective>
+          {stackedCustomSeries.map((item , index)=>(
+            <SeriesDirective key={index} {...item}  />
+          ))}
+        </SeriesCollectionDirective>
+        
+        <Inject services={[Legend, Category, Tooltip, StackingColumnSeries , DataLabel]} />
+      </ChartComponent>
+
+    </div>
   )
 }
+export default Stacked; 
 
-export default Stacked
+
+
+
+
